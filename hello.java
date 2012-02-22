4678da17a9d223619bff3a5a4f161b9480f4aa71
Derek Escue <derek.escue@gmail.com>
2012-02-21 21:45:41 -0600

    first commit

---
+++ hello.java
@@ -0,0 +1,10 @@
+/**
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ * Derek Escue
+ */
+class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+    }
+}
